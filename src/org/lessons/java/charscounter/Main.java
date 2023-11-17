40cf07882b36232c37cd40fdb7fdac540ec4cb15
SimoneVilardo <simovila95@gmail.com>
2023-11-17 14:13:47 +0100

    esercizio completato

--- src/org/lessons/java/charscounter/Main.java
+++ src/org/lessons/java/charscounter/Main.java
@@ -1,9 +1,35 @@
 package org.lessons.java.charscounter;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Inserisci una parola = ");
+		String parola = in.nextLine().toLowerCase();
+		
+		in.close();
+		
+		Map<Character, Integer> occorrenza = new HashMap<>();
+		
+		
+		for (Character c : parola.toLowerCase().toCharArray()) {
+			
+			if (occorrenza.containsKey(c)) {
+				
+				occorrenza.put(c, occorrenza.get(c) + 1);
+			} else {
+				
+				occorrenza.put(c, 1);
+			}
+		}
+		
+		System.out.println(occorrenza);
 
 	}
 
